312cd0ddb469e3c6647e49984f6bae823892f013
Sam <119711271+sammmcm@users.noreply.github.com>
2025-05-03 16:48:23 -0600

    update baja.h

--- include/baja.h
+++ include/baja.h
@@ -1,10 +1,10 @@
 #ifndef BAJA_H_INCLUDED
 #define BAJA_H_INCLUDED
 
+alumno busquedaPorMatricula(alumno &estructura, long &bajaMat);
+alumno busquedaPorNombre(alumno &estructura, string &elimNom);
 void bajaParcial(alumno &listaAltas, alumno &listaBajas, alumno &pila);
 void deshacerBajaParcial(alumno &pila, alumno &listaAltas, alumno &listaBajas);
-alumno bajaPorMatricula(alumno &listaBajas, long &bajaMat);
-alumno bajaPorNombre(alumno &listaBajas, string &elimNom);
 void bajaTotal(alumno &listaBajas, alumno &pila);
 
 #endif // BAJA_H_INCLUDED
