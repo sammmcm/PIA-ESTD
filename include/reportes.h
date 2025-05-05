47371f9f76ecfb1c5f75168bc59cb7be80ad3574
Sam <119711271+sammmcm@users.noreply.github.com>
2025-05-04 20:53:25 -0600

    update reportes.h - sam

--- include/reportes.h
+++ include/reportes.h
@@ -4,11 +4,8 @@
 #include "alta.h"
 
 void imprimirAlumnosAprobados(const alumno &listaAltas);
-
 void imprimirPorcentajes(const alumno &listaAltas);
-
 void imprimirDatosGenerales(const alumno &listaAltas);
-
 void imprimirAlumnosInactivos(const alumno &listaBajas);
 
 #endif
