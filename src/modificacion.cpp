47bf380e33c1b723df57eba031367b97e7429049
Sam <119711271+sammmcm@users.noreply.github.com>
2025-05-09 00:57:39 -0600

    update modificacion.cpp

--- src/modificacion.cpp
+++ src/modificacion.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <string>
-#include "modificacion.h"
-#include "validaciones.h"
 #include "alta.h"
 #include "inscripcion.h"
+#include "modificacion.h"
+#include "validaciones.h"
 
 using namespace std;
+
 ostream& operator<<(ostream& os, const insc& estado) {
     switch(estado) {
         case esperando: os << "esperando"; break;
@@ -30,83 +31,76 @@ void modificarAlumno(alumno encontrado) {
 
     int opcionMod;
     do {
-        cout << "\nASPECTO A MODIFICAR: " << endl;
-        cout << "1. Nombre" << endl;
-        cout << "2. Edad" << endl;
-        cout << "3. Promedio" << endl;
-        cout << "4. Calle" << endl;
-        cout << "5. Colonia" << endl;
-        cout << "6. Municipio" << endl;
-        cout << "7. Número de domicilio" << endl;
-        cout << "8. Código postal" << endl;
-        cout << "9. Salir" << endl;
+        cout << "\nQue desea modificar?" << endl;
+        cout << "1. Nombre." << endl;
+        cout << "2. Edad " << endl;
+        cout << "3. Promedio." << endl;
+        cout << "4. Calle." << endl;
+        cout << "5. Colonia." << endl;
+        cout << "6. Municipio." << endl;
+        cout << "7. Numero de domicilio." << endl;
+        cout << "8. Codigo postal." << endl;
+        cout << "9. Salir." << endl;
 
         do {
-            cout << "Opcion: ";
-        } while (!validarInt(opcionMod) && cout << "Error: debe ingresar un numero válido.\n");
+            cout << "Elija una opcion: ";
+        } while (!validarInt(opcionMod) && cout << "Error: La opcion debe ser entero y contener solo numeros.\n");
 
         switch (opcionMod) {
             case 1: {
                 do {
-                    cout << "Nuevo nombre: "; getline(cin, encontrado->nombre);
-                } while (!validarString(encontrado->nombre) && cout << "Error: El nombre debe ser solo letras." << endl);
-                cout << "El nombre ha sido cambiado a "<< encontrado->nombre << endl;
+                    cout << endl << "Ingrese el nuevo nombre: "; getline(cin, encontrado->nombre);
+                } while (!validarString(encontrado->nombre) && cout << "Error: El nombre debe estar conformado solo por letras." << endl);
                 break;
             }
             case 2: {
                 do {
-                    cout << "Nueva edad: ";
-                } while (!validarInt(encontrado->edad) && cout << "Error: edad invalida.\n");
-                cout << "La edad a sido cambiada a "<< encontrado->edad << endl;
+                    cout << "Ingrese la nueva edad: ";
+                } while (!validarInt(encontrado->edad) && cout << "Error: La edad debe ser positiva y contener solo numeros.\n");
                 break;
             }
             case 3: {
                 do {
-                    cout << "Nuevo promedio: ";
-                } while (!validarDouble(encontrado->promedio) && cout << "Error: promedio invalido.\n");
-                cout << "El promedio ha sido cambiado a "<< encontrado->promedio << endl;
+                    cout << "Ingrese el nuevo promedio: ";
+                } while (!validarDouble(encontrado->promedio) && cout << "Error: El promedio debe ser positiva y contener solo numeros.\n");
                 break;
             }
             case 4: {
                 do {
-                    cout << "Nueva calle: "; getline(cin, encontrado->address.calle);
-                } while (!validarString(encontrado->address.calle) && cout << "Error: La calle debe ser solo letras." << endl);
-                cout << "La calle ha sido cambiada a "<< encontrado->address.calle << endl;
+                    cout << "Ingrese la nueva calle: "; getline(cin, encontrado->address.calle);
+                } while (!validarString(encontrado->address.calle) && cout << "Error: La calle debe estar conformado solo por letras." << endl << endl);
                 break;
             }
             case 5: {
                 do {
-                    cout << "Nueva colonia: "; getline(cin, encontrado->address.colonia);
-                } while (!validarString(encontrado->address.colonia) && cout << "Error: La colonia debe ser solo letras." << endl);
-                cout << "La colonia ha sida cambiado a "<< encontrado->address.colonia << endl;
+                    cout << "Ingrese la nueva colonia: "; getline(cin, encontrado->address.colonia);
+                } while (!validarString(encontrado->address.colonia) && cout << "Error: La colonia debe estar conformado solo por letras." << endl << endl);
                 break;
             }
             case 6: {
                 do {
-                cout << "Nuevo municipio: "; getline(cin, encontrado->address.municipio);
-                } while (!validarString(encontrado->address.municipio) && cout << "Error: El municipio debe ser solo letras." << endl);
-                cout << "El municipio ha sido cambiado a "<< encontrado->address.municipio << endl;
+                    cout << "Ingrese el nuevo municipio: "; getline(cin, encontrado->address.municipio);
+                } while (!validarString(encontrado->address.municipio) && cout << "Error: El municipio debe estar conformado solo por letras." << endl << endl);
                 break;
             }
             case 7: {
                 do {
-                    cout << "Nuevo numero de domicilio: ";
-                } while (!validarInt(encontrado->address.numero) && cout << "Error: numero invalido.\n");
-                cout << "El numero de domiciliio ha sido cambiado a "<< encontrado->address.numero << endl;
+                    cout << "Ingrese el nuevo numero de domicilio: ";
+                } while (!validarInt(encontrado->address.numero) && cout << "Error: El numero debe ser positivo y contener solo numeros.\n");
                 break;
             }
             case 8: {
                 do {
-                    cout << "Nuevo codigo postal: ";
-                } while (!validarInt(encontrado->address.cp) && cout << "Error: CP invalido.\n");
-                cout << "El codigo postal ha sido cambiado a "<< encontrado->address.cp << endl;
+                    cout << "Ingrese el nuevo codigo postal: ";
+                } while (!validarInt(encontrado->address.cp) && cout << "Error: El CP debe ser positivo y contener solo numeros.\n");
                 break;
             }
             case 9:
-                cout << "Saliendo del menu de modificacion." << endl;
+                cout << "Saliendo del submenu..." << endl;
                 break;
             default:
-                cout << "Opcion invalida." << endl;
+                cout << "Error: Opcion incorrecta." << endl;
+                break;
         }
     } while (opcionMod != 9);
 }
