6cac77e7868b7c1822f667ebaf89bd0c7f83db3f
Sam <119711271+sammmcm@users.noreply.github.com>
2025-05-11 16:33:37 -0600

    grupos.cpp - lógica, validaciones, formato

--- src/grupos.cpp
+++ src/grupos.cpp
@@ -1,56 +1,53 @@
 #include <iostream>
 #include "alta.h"
 #include "grupos.h"
+#include "listas.h"
+#include "validaciones.h"
 
 using namespace std;
 
-int contarAlumnosActivos(alumno lista) {
+int contarAlumnosActivos(alumno listaAlta) {
     int contador = 0;
-    while (lista != nullptr) {
-        if (lista->situacion == alta) {
+    while (listaAlta != nullptr) {
+        if (listaAlta->situacion == alta) {
             contador++;
         }
-        lista = lista->sig;
+        listaAlta = listaAlta->sig;
     }
+
     return contador;
 }
 
-void crearGrupos(alumno lista) {
+void crearGrupos(alumno listaAlta) {
+    if (listaVacia(listaAlta)) {
+        cout << "No hay alumnos para crear grupos." << endl << endl;
+        return;
+    }
+
     int numGrupos;
-    cout << "\nCantidad de grupos a crear: ";
-    cin >> numGrupos;
+    do {
+        cout << "\nIngrese la cantidad de grupos a crear: ";
+    } while ((!validarInt(numGrupos) || numGrupos == 0) && cout << "Error: La cantidad debe ser mayor a 0 y contener solo numeros." << endl);
 
-    int totalAlumnos = contarAlumnosActivos(lista);
-
-    if (numGrupos <= 0 || totalAlumnos == 0) {
-        cout << "\n - Debe haber al menos 1 alumno para crear un grupo -\n" << endl;
+    int totalAlumnos = contarAlumnosActivos(listaAlta);
+    if (totalAlumnos < numGrupos) {
+        cout << "Error: No se pueden crear " << numGrupos << " grupos con " << totalAlumnos << " alumnos. Intentelo de nuevo." << endl << endl;
         return;
     }
 
     int base = totalAlumnos / numGrupos;
     int extra = totalAlumnos % numGrupos;
-
-    alumno alumnos[totalAlumnos];
-    int i = 0;
-
-    alumno actual = lista;
-    while (actual != nullptr) {
-        if (actual->situacion == alta) {
-            alumnos[i++] = actual;
-        }
-        actual = actual->sig;
-    }
-
-    int index = 0;
-
+    
+    alumno actual = listaAlta;
     for (int g = 0; g < numGrupos; g++) {
         int cantidadGrupo = base + (extra > 0 ? 1 : 0);
-        cout << "\nGrupo " << (g + 1) << ":" << endl;
-        for (int j = 0; j < cantidadGrupo; j++) {
-            cout << "Matricula: " << alumnos[index]->matricula
-                 << ", Nombre: " << alumnos[index]->nombre << endl;
-            index++;
+        cout << "\nGrupo " << (g + 1) << endl;
+        while (actual != nullptr && cantidadGrupo > 0) {
+            cout << "" << actual->nombre << " " << actual->matricula << endl;
+            actual = actual->sig;
+            cantidadGrupo--;
         }
         if (extra > 0) extra--;
     }
+    cout << endl;
 }
