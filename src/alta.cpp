b6f75438e1c51a7613fe2ea98649671489d76f54
Sam <119711271+sammmcm@users.noreply.github.com>
2025-05-11 21:41:36 -0600

    alta.cpp - num y cp

--- src/alta.cpp
+++ src/alta.cpp
@@ -10,13 +10,13 @@ alumno altaAlum() {
     string entrada;
     do {
         cout << endl << "Ingrese la matricula: ";
-    } while (!validarLong(nuevo->matricula) && cout << "Error: La matricula debe ser un numero positivo de 7 digitos" << endl);
+    } while (!validarLong(nuevo->matricula) && cout << "Error: La matricula debe ser un numero positivo de 7 digitos." << endl);
     do {
         cout << endl << "Ingrese el nombre: "; getline(cin, nuevo->nombre);
     } while (!validarString(nuevo->nombre) && cout << "Error: El nombre debe estar conformado solo por letras." << endl);
     do {
         cout << endl << "Ingrese la edad: ";
-    } while ((!validarInt(nuevo->edad) || nuevo->edad < 5 || nuevo->edad > 100) && cout << "Error: La edad debe ser un numero positivo entre 5 y 100 años." << endl);
+    } while ((!validarInt(nuevo->edad) || nuevo->edad < 5 || nuevo->edad > 100) && cout << "Error: La edad debe ser un numero positivo entre 5 y 100 anos." << endl);
     do {
         cout << endl << "Ingrese el promedio general: ";
     } while ((!validarDouble(nuevo->promedio) || nuevo->promedio > 100) && cout << "Error: El promedio debe ser un numero positivo entre 0 y 100." << endl);
@@ -33,10 +33,10 @@ alumno altaAlum() {
     } while (!validarString(nuevo->address.municipio) && cout << "Error: El municipio debe estar conformado solo por letras." << endl << endl);
     do {
         cout << "Numero: ";
-    } while ((!validarInt(nuevo->address.numero) || nuevo->address.numero < 100 && nuevo->address.numero > 999) && cout << "Error: El numero debe ser un numero positivo de 3 digitos." << endl << endl);
+    } while ((!validarInt(nuevo->address.numero) || nuevo->address.numero < 100 || nuevo->address.numero > 999) && cout << "Error: El numero debe ser un numero positivo de 3 digitos." << endl << endl);
     do {
         cout << "CP: ";
-    } while ((!validarInt(nuevo->address.cp) || nuevo->address.cp < 10000 && nuevo->address.cp > 99999)  && cout << "Error: El CP debe ser un numero positivo de 5 digitos." << endl << endl);
+    } while ((!validarInt(nuevo->address.cp) || nuevo->address.cp < 10000 || nuevo->address.cp > 99999)  && cout << "Error: El CP debe ser un numero positivo de 5 digitos." << endl << endl);
     nuevo->inscripcion = esperando;
 
     return nuevo;
